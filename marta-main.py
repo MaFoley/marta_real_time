5028f9a97bdf0916e2a3a6dbc8d312bb90ff920f
Malcolm Foley <mfoley.1026@gmail.com>
2024-05-07 23:23:01 -0400

    add real-time vehicle positions to map

--- marta-main.py
+++ marta-main.py
@@ -1,5 +1,5 @@
 from google.protobuf.gtfs_realtime_pb2 import * 
-import collections,sqlite3
+import collections, geopy.geocoders, sqlite3
 messageFeed = FeedMessage()
 conn = sqlite3.connect('marta_schedule.sqlite')
 conn.row_factory = sqlite3.Row
@@ -34,7 +34,7 @@ class VehicleTripUpdate:
                 ,self._occupancy_status)
 
 
-with open('tripupdates.pb',"rb") as f:
+with open('vehiclepositions.pb',"rb") as f:
     messageFeed.ParseFromString(f.read())
 sql = 'INSERT INTO real_time_vehicles VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'
 for e in messageFeed.entity:
@@ -42,9 +42,11 @@ for e in messageFeed.entity:
     cur.execute(sql,vehicleTripRecord.getTuple())
     #if e.vehicle is not None and int(rte_id) <= 21622:
     #print(e,file=open("marta_real_trips.txt","a"))
-Location = collections.namedtuple('Location',['latitude','longitude'])
-loc = Location(33.871590,-84.381127)
-def get_nearby_stops(l: Location, rng: float):
+address_string = '4266 Roswell Rd NE Atlanta, GA 30342'
+geolocator = geopy.geocoders.Nominatim(user_agent='marta-real-time')
+loc = geolocator.geocode(address_string)
+
+def get_nearby_stops(l: geopy.location.Location, rng: float):
     """rng denoted in degrees of lat/long"""
     lower_latitude = l.latitude - rng
     upper_latitude =  l.latitude + rng
@@ -60,11 +62,35 @@ stop_sql = get_nearby_stops(loc,0.005)
 cur.execute(stop_sql)
 stops = cur.fetchall()
 print(stops[:10])
+route_id_sql = f'''SELECT DISTINCT route_id FROM ((trips 
+INNER JOIN stop_times ON trips.trip_id = stop_times.trip_id )
+INNER JOIN stops ON stop_times.stop_id = stops.stop_id) WHERE stops.stop_id = ?'''
+chosen_stop_id = stops[0]['stop_id']
+cur.execute(route_id_sql, (chosen_stop_id,))
+route = cur.fetchall()
+vehicle_position_sql = '''
+SELECT vehicle_id, route_id, position_latitude, position_longitude, vehicle_label
+FROM real_time_vehicles
+WHERE route_id = ? '''
+cur.execute(vehicle_position_sql,(route[0]['route_id'],))
+vehicles = cur.fetchall()
 conn.close()
 import folium
 def draw_map():
-    base_map = folium.Map(tiles='CartoDB positron',location= loc, zoom_start=12)
+    base_map = folium.Map(tiles='CartoDB positron',location= (loc.latitude, loc.longitude), zoom_start=13)
     radius = 10
+    for veh in vehicles:
+        folium.CircleMarker(
+            location=[veh["position_latitude"],veh["position_longitude"]],
+            radius=radius,
+            color="green",
+            stroke=False,
+            fill=True,
+            fill_opacity=0.6,
+            opacity=1,
+            popup="{} pixels".format(radius),
+            tooltip=veh["vehicle_label"],
+        ).add_to(base_map)
     for stop in stops:
         folium.CircleMarker(
             location=[stop["stop_lat"],stop["stop_lon"]],
@@ -75,12 +101,11 @@ def draw_map():
             fill_opacity=0.6,
             opacity=1,
             popup="{} pixels".format(radius),
-            tooltip="I am in pixels",
+            tooltip=stop["stop_name"],
         ).add_to(base_map)
     base_map.save('base_map.html')
 draw_map()
 """
-
 import requests
 BASE_URL = 'https://gtfs-rt.itsmarta.com/TMGTFSRealTimeWebService/vehicle'
 pb_file = requests.get(BASE_URL)
